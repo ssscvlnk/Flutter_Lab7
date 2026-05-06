d6c2fbcc79ebcdf47c9ad600760187c6ed74a6ca
alincqeek <pstartceva@yandex.ru>
2026-05-06 14:52:59 +0300

    Update SoundService to use audioplayers for Web and Android

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:slot_machine/slot_machine.dart';
-void main() {
+import 'package:slot_machine/sound_service.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SoundService.init();
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.deepPurple,
         body: SlotMachine(),
-      )
-    )
+      ),
+    ),
   );
 }
